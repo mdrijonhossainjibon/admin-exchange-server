5793767fae8f6663c9852e5d29b79e40695282c3
Md Rijon Hossain Jibon <79151625+mdrijonhossainjibon@users.noreply.github.com>
2023-10-18 10:30:05 +0600

    Update confing.jsx

--- src/api/confing.jsx
+++ src/api/confing.jsx
@@ -1,8 +1,8 @@
 
 
 export const API_CALL_ENDPOINT = {
-    serverurl: `https://server-jaqg.onrender.com`,
-    adminserver : `https://server-jaqg.onrender.com`,
+    serverurl: `https://web-godl.onrender.com`,
+    adminserver : `https://web-godl.onrender.com`,
     base_api_verson : '/api/v1',
     methed_post: 'post',
     methed_get: 'get',
